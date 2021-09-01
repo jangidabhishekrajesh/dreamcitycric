2bb580e4d35553e2f0fa04788c1bd13a5be9ddad
Abhishek Jangid <83941830+jangidabhishekrajesh@users.noreply.github.com>
2021-09-01 21:36:28 +0530

    Update admission.jsx

--- src/admission.jsx
+++ src/admission.jsx
@@ -25,11 +25,11 @@ let Admission = () => {
                     <h2 className="text-center text-primary p-2">News And More</h2>
                 </div>
                 <div className="row" id="shodowContainer">
-                    <div className="col-md-8 col-sm-12">
+                    <div className="col-md-12 col-sm-12">
                         <h3 className="text-light p-3 mt-4">
                             Our Culture & Little Bit About Work!
                         </h3>
-                        <div className="cardBlur p-5">
+                        <div className="cardBlur p-5 mb-5">
                             <h3 className="mb-5">Values Of DCCA!</h3>
                             <h5>RESPECT</h5>
                             <small>Respect for others is one of the basic requirements of democratic life and the capacity to live in society. It affects the individual and has both a social and a collective dimension. Respect is a fundamental part of a sport, for sport generates situations of tension that have to be resolved almost instantaneously. There is, therefore, a need, above rules and standards, for this value to be present in order to guarantee cordial and correct interrelations between people. In sport, respect is needed in similar situations to those where it is demanded in wider society, but the intensity of the moment implies that the presence or absence of respect can be very obvious and can affect the development of the situation or event.</small>
@@ -47,25 +47,27 @@ let Admission = () => {
                             <small>Often when athletes, and people in general, achieve the goals they have set themselves, they start feeling superior in physical, moral, economic and other terms, and this leads them to forget certain values that have in all probability helped them to achieve those very goals: effort, perseverance, self-control, etc. We are all aware of how DCCA has dealt with its victories of recent years, and how it has always shown humility (and also such values as effort, rigour, responsibility, and others) to win respect in its field, and among its opponents, and generally everyone. Humility probably consists of knowing how to be clear about one’s values, and to maintain them and defend them even in situations where you are clearly superior and more successful.</small>
                         </div>
                     </div>
-                    <div className="col-md-4 col-sm-12">
-                        <h3 className="text-light p-3 mt-4">
-                            Recent News
-                        </h3>
-                        {admission && admission.map((item, idx) => 
-                        <div key= {idx}>
-                            <div className="cardBlur m-1 p-1">
-                                <ul style={{listStyle:'none'}}>
-                                    <b>Head Line</b>
-                                    <li><small>{item.title}</small></li>
-                                    <b>For More! Click Link</b>
-                                    <li><small><a href={item.url} target="_blank" rel="noreferrer">{item.url}</a></small></li>
-                                    <img className="urlToImg" src={item.urlToImage} alt="img" width="100%"/>
-                                    <b>Date & Time</b >
-                                    <li><small>{item.publishedAt}</small></li>
-                                </ul>
-                            </div>
-                        </div>)}
-                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <h3 className="p-3 mt-4">
+                        Recent News
+                    </h3>
+                    {admission && admission.map((item, idx) => 
+                    <div className="col-md-4 col-sm-12" key= {idx}>
+                        <div className="cardBlur m-1 p-2">
+                            <ul style={{listStyle:'none'}}>
+                                <b>Head Line</b>
+                                <li><small>{item.title}</small></li>
+                                <b>For More! Click Link</b>
+                                <li><small><a href={item.url} target="_blank" rel="noreferrer">{item.url}</a></small></li>
+                                <img className="urlToImg" src={item.urlToImage} alt="img" style={{width:"100%", height:"70%"}}/>
+                                <b>Date & Time</b >
+                                <li><small>{item.publishedAt}</small></li>
+                            </ul>
+                        </div>
+                    </div>)}
                 </div>
             </div>
         </>
